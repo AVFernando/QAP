3b3d5c1e36969738b4e0d0a890296ae9bd3cd86a
Fernando Arellano <arellano.fernando.v@gmail.com>
2024-04-17 00:10:51 -0400

    Greedy listo

--- Modulos/QAP.py
+++ Modulos/QAP.py
@@ -1,44 +1,83 @@
-import random
 import numpy as np
+import random
+from copy import deepcopy
+from enum import Enum
 
 class QAP_Instance:
-    def __init__(self):
-        n = 3 # Tamaño de las matrices
-        self.n = n
-        self.flow_matrix = np.random.randint(1, 10, size=(n, n)) # Matriz de flujo
-        self.distance_matrix = np.random.randint(1, 10, size=(n, n)) # Matriz de distancia
-        self.assignment_matrix = np.zeros((n, n)) # Matriz de asignacion (inicializada a 0)
+    def __init__(self, flow, distance):
+        self.flow_matrix = flow
+        self.distance_matrix = distance
         print("\nMatrices generadas aleatoriamente:")
         print("Matriz de flujo:")
         print(self.flow_matrix)
         print("\nMatriz de distancia:")
         print(self.distance_matrix)
-        print("\nMatriz de asignacion:")
-        print(self.assignment_matrix)
 
 class QAP_State:
-    def __init__(self, inst_info, assigned=None):
-        self.assigned = assigned if assigned is not None else []
-        self.not_assigned = set(range(len(inst_info.assignment_matrix))) - set(self.assigned)
+    def __init__(self, instance, assigned=[0]):
+        self.instance = instance
+        self.assigned = assigned
+        self.not_assigned = list(set(range(len(instance.flow_matrix))) - set(self.assigned))
         self.all_assigned = len(self.not_assigned) == 0
-        self.inst_info = inst_info
-        self.cost = self.calculate_cost()
+        self.cost = 0
+        self.update_cost()
 
-    def calculate_cost(self):
-        cost_matrix = np.multiply(self.inst_info.distance_matrix, self.inst_info.assignment_matrix)
-        cost = np.sum(np.multiply(self.inst_info.flow_matrix, cost_matrix))
-        return int(cost)
+    def calculate_cost(self, i, j):
+        place1 = self.assigned[i]
+        place2 = self.assigned[j]
+
+        flow_cost = self.instance.flow_matrix[place1, place2]
+        distance_cost = self.instance.distance_matrix[i, j]
+
+        return flow_cost * distance_cost
+
+
+    def update_cost(self):
+        n = len(self.assigned)
+        if n > 1:
+            cost = sum(self.calculate_cost(i, j) for i in range(n) for j in range(n) if i != j)
+            self.cost = cost
+        else:
+            self.cost = 0
+
+
+    def __deepcopy__(self, memo):
+        copy_instance = type(self)(instance=self.instance, assigned=deepcopy(self.assigned))
+        return copy_instance
+
+    def __str__(self):
+        return f"Asignacion en la lista: {self.assigned}\nCosto de la asignacion: {self.cost}"
+
+class move_type(Enum):
+    CONSTRUCTIVE = 1
+    SWAP = 2
+    TWO_OPT = 3
+    RELOCATION = 4
 
 class QAP_Environment():
     @staticmethod
-    def gen_actions(state, type, shuffle=False):
-        if type == "constructive":
-            actions = [("constructive", assigned) for assigned in state.not_assigned]
-        elif type == "swap":
+    def gen_actions(state, move_type, shuffle=False):
+        if move_type == move_type.CONSTRUCTIVE:
+            actions = [(move_type.CONSTRUCTIVE, place) for place in state.not_assigned]
+
+        elif move_type == move_type.SWAP:
             n_len = len(state.assigned)
-            actions = [(type, (i, j)) for i in range(n_len) for j in range(i + 1, n_len)]
+            actions = [(move_type.SWAP, (i, j)) for i in range(n_len) for j in range(i + 1, n_len)]
+
+        elif move_type == move_type.TWO_OPT:
+            n_len = len(state.assigned)
+            actions = [(move_type.TWO_OPT, (i, j)) for i in range(n_len) for j in range(i - 1, i + 2) if j != i]
+
+        elif move_type == move_type.RELOCATION:
+            n_len = len(state.assigned)
+            actions = []
+            for i in range(n_len):
+                for j in range(n_len):
+                    if i != j:
+                        actions.append((move_type, (i, j)))
+
         else:
-            raise NotImplementedError(f"Tipo de accion '{type}' no implementado para QAP")
+            raise NotImplementedError(f"Tipo de accion '{move_type}' no implementado para QAP")
 
         if shuffle:
             random.shuffle(actions)
@@ -48,99 +87,167 @@ class QAP_Environment():
 
     @staticmethod
     def state_transition(state, action):
-        new_state = QAP_State(state.inst_info, assigned=state.assigned.copy())
+        if action[0] == move_type.CONSTRUCTIVE and not state.all_assigned:
+            state.assigned.append(action[1])
+            state.not_assigned.remove(action[1])
 
-        if action[0] == "constructive" and not state.all_assigned:
-            new_state.assigned.append(action[1])
-            new_state.not_assigned.remove(action[1])
-            new_state.inst_info.assignment_matrix[action[1], action[1]] = 1
-            new_state.cost = new_state.calculate_cost()
+            if len(state.not_assigned) == 0:
+                state.update_cost()
+                state.all_assigned = True
 
-        elif len(state.not_assigned) == 0:
-            new_state.assigned.append(state.assigned[0])
-            new_state.inst_info.assignment_matrix[state.assigned[0], state.assigned[0]] = 1
-            new_state.cost = new_state.calculate_cost()
-            new_state.all_assigned = True
-
-        elif action[0] == "swap":
+        elif action[0] == move_type.SWAP and state.all_assigned == True:
+            print("\nMomento de hacer swap o two-opt")
             i, j = action[1]
-            if i not in new_state.assigned or j not in new_state.assigned:
+            if i not in state.assigned or j not in state.assigned:
                 raise ValueError(f"Las instalaciones {i} y/o {j} no están asignadas.")
+            i_idx = state.assigned.index(i)
+            j_idx = state.assigned.index(j)
+            state.assigned[i_idx] = j
+            state.assigned[j_idx] = i
+            state.update_cost()
 
-            i_idx = new_state.assigned.index(i)
-            j_idx = new_state.assigned.index(j)
-            new_state.assigned[i_idx] = j
-            new_state.assigned[j_idx] = i
-            new_state.inst_info.assignment_matrix[i, j] = 1
-            new_state.inst_info.assignment_matrix[j, i] = 1
-            new_state.cost = new_state.calculate_cost()
+        elif action[0] == move_type.RELOCATION and state.all_assigned == True:
+            print("\nMomento de hacer relocation")
+            i, j = action[1]
+            if i not in state.assigned or j not in state.assigned:
+                raise ValueError(f"Las instalaciones {i} y/o {j} no están asignadas.")
+            i, j = action[1]
+            elem = state.assigned.pop(i)
+            state.assigned.insert(j, elem)
+            state.update_cost()
 
-        else:  # Acción no válida
-            raise NotImplementedError(f"Tipo de acción '{action[0]}' no implementado para QAP")
+        else:
+            raise NotImplementedError(f"Tipo de accion '{action[0]}' no implementado para QAP")
 
-        return new_state
+        return state
 
+    def calculate_cost_after_action(state, action):
+        if action[0] == move_type.SWAP:
+            k, l = action[1]
 
+            # Swap the facilities in the assignment array
+            state.assignment_array[k], state.assignment_array[l] = \
+                state.assignment_array[l], state.assignment_array[k]
 
-    def calculate_cost_after_action(state, assigned):
-        temp_assignment_matrix = np.copy(state.inst_info.assignment_matrix)
-        not_assigned_locations = np.where(temp_assignment_matrix[:, assigned] == 0)[0]
-        if len(not_assigned_locations) == 0:
-            raise ValueError(f"No hay ubicaciones disponibles para la instalacion {assigned}.")
-        
-        temp_assignment_matrix[assigned, not_assigned_locations[0]] = 1
-        temp_state = QAP_State(state.inst_info, assigned=state.assigned + [assigned])
-        temp_state.inst_info.assignment_matrix = temp_assignment_matrix
-        cost = temp_state.calculate_cost()
-        
-        return cost
+            # Calculate the change in cost caused by the swap
+            old_cost = state.cost
+
+            # Calculate the change in cost for the swapped facilities
+            change_in_cost = 0
+            n = len(state.assignment_array)
+            for i in range(n):
+                if i != k and i != l:  # Exclude the swapped facilities
+                    facility1 = state.assignment_array[i]
+                    facility2_k = state.assignment_array[k]
+                    facility2_l = state.assignment_array[l]
+
+                    location1 = i
+                    location2_k = k
+                    location2_l = l
+
+                    # Calculate the change in cost for swapping k and i with l and i
+                    change_in_cost += (state.instance.flow_matrix[facility1, facility2_l] - 
+                                       state.instance.flow_matrix[facility1, facility2_k]) * \
+                                        state.instance.distance_matrix[location1, location2_k] + \
+                                        (state.instance.flow_matrix[facility1, facility2_k] - 
+                                       state.instance.flow_matrix[facility1, facility2_l]) * \
+                                        state.instance.distance_matrix[location1, location2_l]
+
+            # Update the cost with the change
+            new_cost = old_cost + change_in_cost
+            state.cost = new_cost
+
+            # Swap back to the original assignment array
+            state.assignment_array[k], state.assignment_array[l] = \
+                state.assignment_array[l], state.assignment_array[k]
+
+            return new_cost
+
+        return state.cost
 
 def evalConstructiveActions(state, env):
     evals = []
-    for action in env.gen_actions(state, "constructive"):
-        cost_after_action = env.calculate_cost_after_action(state, action[1])
-        evals.append((action, cost_after_action))
+    n = len(state.assigned)
+    for action in env.gen_actions(state, move_type.CONSTRUCTIVE):
+        place2 = action[1]
+        
+        if place2 in state.assigned:
+            continue
+        cost = 0
+        for i, place1 in enumerate(state.assigned):
+            cost += state.instance.flow_matrix[place1, place2] * \
+                    state.instance.distance_matrix[i, n]
+        evals.append((action, -cost))
     return evals
 
-class GreedyAgent():
-    def __init__(self, evals):
-        self.evals = evals
+class GreedyAgent:
+    def __init__(self, eval_actions):
+        self.eval_actions = eval_actions
 
     def select_action(self, evals):
         return max(evals, key=lambda x: x[1])[0]
 
     def action_policy(self, state, env):
-        evals = self.evals(state, env)
-        if len(evals)==0: return None
-
-        # Seleccionar la acción que maximiza su evaluación
+        evals = self.eval_actions(state, env)
+        if len(evals) == 0:
+            return None
         return self.select_action(evals)
 
-# Crear una instancia del problema QAP
-inst_info = QAP_Instance()
-# Crear un estado inicial
-current_state = QAP_State(inst_info, assigned=[])
-# Crear el entorno
-env = QAP_Environment
-# Crear un agente Greedy
-greedy_agent = GreedyAgent(evalConstructiveActions)
+    def __deepcopy__(self, memo):
+        copy_instance = type(self)(eval_actions=self.eval_actions)
+        return copy_instance
 
-# Imprimir el estado inicial
-print("\nEstado inicial:")
-print("Instalaciones asignadas:", current_state.assigned)
-print("Instalaciones no asignadas:", current_state.not_assigned)
-print("Costo inicial:", current_state.cost)
+class LocalSearchAgent(GreedyAgent):
+    def __init__(self, action_type, first_improvement=True):
+        self.action_type = action_type
+        self.first_improvement = first_improvement
 
-# Actuar con el agente Greedy
-while not current_state.all_assigned:
-    action = greedy_agent.action_policy(current_state, env)
+    def eval_actions(self, state, env):
+        current_cost = state.cost
+        evals = []
+        
+        for action in env.gen_actions(state, self.action_type, shuffle=True):
+            new_cost = env.calculate_cost_after_action(state, action)
+            if new_cost < current_cost:
+                evals.append((action, new_cost))
+                if self.first_improvement:
+                    return evals
+        return evals
+    
+    def __deepcopy__(self, memo):
+        new_instance = type(self) (
+            action_type = self.action_type,
+            first_improvement = self.first_improvement
+        )
+        return new_instance
+
+#--------------------------------------------------------------------------------
+flow = np.array([[0, 3, 1, 2], 
+                 [3, 0, 1, 1], 
+                 [1, 1, 0, 4], 
+                 [2, 1, 4, 0]])
+
+distance = np.array([[0,    22,   53, 1000],
+                     [22,   0,    40, 1000],
+                     [53,   40,   0,  55],  
+                     [1000, 1000, 55, 0]])  
+
+env = QAP_Environment()
+instance = QAP_Instance(flow, distance)
+current_state = QAP_State(instance)
+
+agent = GreedyAgent(evalConstructiveActions)
+while True:
+    action = agent.action_policy(current_state, env)
+    if action is None:
+        break
     current_state = env.state_transition(current_state, action)
-    print("\nAccion tomada:", action)
-    print("Instalaciones asignadas:", current_state.assigned)
-    print("Instalaciones no asignadas:", current_state.not_assigned)
-    print("Costo despues de la accion:", current_state.cost)
+print(f"\nEstado Final:\n{current_state}\n")
 
-print("\nEstado final:")
-print("Instalaciones asignadas:", current_state.assigned)
-print("Instalaciones no asignadas:", current_state.not_assigned)
-print("Costo final:", current_state.cost)
+agent = LocalSearchAgent(move_type.SWAP, first_improvement=True)
+while True:
+    action = agent.action_policy(current_state, env)
+    if action is None:
+        break
+current_state = env.state_transition(current_state, action)
+print(f"\nEstado Final:\n{current_state}")
